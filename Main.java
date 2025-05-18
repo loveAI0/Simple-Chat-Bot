399f57c13557f097da6f2873b19283cd0d06d5d5
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-18 20:49:26 +0000

    Guessing age module is created.

--- Main.java
+++ Main.java
@@ -16,6 +16,21 @@
         System.out.println("Please, remind me your name.");
         String nameUser = input.nextLine();
         Main.userInfo(nameUser);
+
+        // Age guessing Game
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int rem3 = input.nextInt();
+        int rem5 = input.nextInt();
+        int rem7 = input.nextInt();
+        input.nextLine();
+
+        Main.guessTheAge(rem3, rem5, rem7);
+
+
+
+        
     }
 
     public static void greeting(String botName, int birthYear){
@@ -26,5 +41,10 @@
     public static void userInfo(String nameUser){
         System.out.println("What a great name you have, " + nameUser + "!");
         
+    }
+
+    public static void guessTheAge(int rem3, int rem5, int rem7){
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println(String.format("Your age is %d; that's a good time to start programming!", age));
     }
  }
