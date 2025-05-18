36764aaedc5923aea1ae2bfd05da574be9a5866e
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-18 20:09:36 +0000

    Step 2 is completed.

--- Main.java
+++ Main.java
@@ -10,12 +10,21 @@
 
         System.out.println("Year the bot was created: ");
         int birthYear = input.nextInt();
-
+        input.nextLine(); // consumes extra new line as only number is read.
         Main.greeting(botName, birthYear);
+        // user Info
+        System.out.println("Please, remind me your name.");
+        String nameUser = input.nextLine();
+        Main.userInfo(nameUser);
     }
 
     public static void greeting(String botName, int birthYear){
-        System.out.println("Hello! My name is " + String.format("%s", botName));
-        System.out.println("I was created in " + String.format("%d", birthYear));
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+    }
+
+    public static void userInfo(String nameUser){
+        System.out.println("What a great name you have, " + nameUser + "!");
+        
     }
  }
